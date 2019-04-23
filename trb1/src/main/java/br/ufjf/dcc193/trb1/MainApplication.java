8a5bc00fa98e21053b829e93fd8842006530aef3
Jônatas <jjsfandre@gmail.com>
2019-04-23 17:49:29 -0300

    Incluindo arquivos do projeto

---
+++ trb1/src/main/java/br/ufjf/dcc193/trb1/MainApplication.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.trb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
