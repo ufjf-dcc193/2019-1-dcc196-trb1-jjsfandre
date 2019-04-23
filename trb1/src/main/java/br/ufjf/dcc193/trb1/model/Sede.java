699edec6d12609510a15d00ac362e0c4a8f73093
Jônatas <jjsfandre@gmail.com>
2019-04-23 19:49:15 -0300

    Incluindo packages Model e Repository
    Incluindo inicialmente objeto Sede nesse modelo e no repositório

---
+++ trb1/src/main/java/br/ufjf/dcc193/trb1/model/Sede.java
@@ -0,0 +1,160 @@
+package br.ufjf.dcc193.trb1.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Sede {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String nomeFantasia;
+    private String uf;
+    private String cidade;
+    private String bairro;
+    private String telefone;
+    private String email;
+    private String site;
+
+
+    void Sede() {
+    }
+
+    
+    
+    public Sede(String nomeFantasia,
+    String uf,
+    String cidade,
+    String bairro,
+    String telefone,
+    String email,
+    String site){
+        this.nomeFantasia = nomeFantasia;
+        this.uf = uf;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.telefone = telefone;
+        this.email = email;
+        this.site = site;
+    }
+
+    //#region getters and setters
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nomeFantasia
+     */
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    /**
+     * @param nomeFantasia the nomeFantasia to set
+     */
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    /**
+     * @return the uf
+     */
+    public String getUf() {
+        return uf;
+    }
+
+    /**
+     * @param uf the uf to set
+     */
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    /**
+     * @return the cidade
+     */
+    public String getCidade() {
+        return cidade;
+    }
+
+    /**
+     * @param cidade the cidade to set
+     */
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    /**
+     * @return the bairro
+     */
+    public String getBairro() {
+        return bairro;
+    }
+
+    /**
+     * @param bairro the bairro to set
+     */
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    /**
+     * @return the telefone
+     */
+    public String getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the site
+     */
+    public String getSite() {
+        return site;
+    }
+
+    /**
+     * @param site the site to set
+     */
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    //#endregion
+
+}
---
+++ trb1/src/main/java/br/ufjf/dcc193/trb1/repository/SedeRepository.java
@@ -0,0 +1,11 @@
+package br.ufjf.dcc193.trb1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ufjf.dcc193.trb1.model.Sede;
+
+public interface SedeRepository extends JpaRepository<Sede,Long>{
+
+
+
+}
